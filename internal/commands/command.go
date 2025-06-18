679cfb4ecbea5cbe6c3aa2e3a9ae41c97a62905e
Dung Nguyen Dang <dangdungcntt@gmail.com>
2025-06-18 15:57:32 +0700

    feat: handle echo

--- internal/commands/command.go
+++ internal/commands/command.go
@@ -1,8 +1,10 @@
 package commands
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -16,11 +18,39 @@ func NewCommand(raw string) Command {
 	}
 }
 
+func (c Command) parse() (string, []string, error) {
+	parts := strings.SplitN(c.raw, " ", 2)
+	if len(parts) == 0 {
+		return "", nil, errors.New("empty command")
+	}
+
+	var args []string
+	if len(parts) == 2 {
+		args = strings.Split(parts[1], " ")
+	}
+
+	return parts[0], args, nil
+}
+
 func (c Command) Execute() {
-	switch c.raw {
-	case "exit 0":
-		os.Exit(0)
+	executable, args, err := c.parse()
+	assertNoError(err)
+
+	switch executable {
+	case "exit":
+		code, err := strconv.Atoi(args[0])
+		assertNoError(err)
+		os.Exit(code)
+	case "echo":
+		fmt.Fprint(os.Stdout, args[0], "\n")
 	default:
 		fmt.Println(c.raw + ": command not found")
+	}
+}
+
+func assertNoError(err error) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 }
